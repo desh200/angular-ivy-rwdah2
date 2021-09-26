fd952ae5ab7892eddce38c7284ee802a1c99fe0e
desh200 <umesh.deshmukh35@gmail.com>
2021-09-26 14:36:07 +0530

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component, VERSION } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  templateUrl: './app.component.html',
+  template: `<h2>Hello</h2><h2>Products</h2>`,
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent  {
