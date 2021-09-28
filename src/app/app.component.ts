ab3faf596b47a8fb7562f1d74e5c5ecd71f833b6
desh200 <umesh.deshmukh35@gmail.com>
2021-09-28 12:25:35 +0530

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,14 @@ import { ProductService } from './product.serice';
 
 @Component({
   selector: 'my-app',
-  template: `<h2>Hello</h2><products></products>`,
+  template: `<h2>Hello</h2>
+  <img src="{{imageurl}}"/>
+  <button class= "btn btn-primary">Save</button>
+  <products></products>`,
   styleUrls: ['./app.component.css'],
   providers:[ProductService]
 })
 export class AppComponent {
   name = 'My Angular ' + VERSION.major;
+  imageurl = "https://www.w3schools.com/html/pic_mountain.jpg";
 }
